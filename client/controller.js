1b38a5202e256e209caa2939ac5390bbcf1da18e
Juliana Hock <jhock@mills.edu>
2017-04-13 02:39:05 +0000

    added client side

--- client/controller.js
+++ client/controller.js
@@ -9,5 +9,49 @@ function PoetController($scope, $http) { //http://stackoverflow.com/questions/13
         .then(function (result) {
             $scope.poems = result.data;
         });
+        
+    $http.get('/api/users')
+        .then(function (result) {
+            $scope.users = result.data;
+        });
+        
+    $http.get('/api/reviews')
+        .then(function (result) {
+            $scope.reviews = result.data;
+        });        
+        
+    $scope.addPoem = function() {
+        $http.post('/api/poem', $scope.newPoem)
+            .then(function (result) {
+               if(result)
+               {
+                   
+               }
+            });
+	}
+	
+	$scope.addUser = function() {
+        $http.post('/api/user', $scope.user)
+            .then(function (result) {
+               if(result)
+               {
+                  $scope.$apply();      //http://stackoverflow.com/questions/12304728/how-can-i-tell-angularjs-to-refresh    
+               }
+            });
+	}
+	
+	$scope.logIn = function() {
+        $http.get('/api/users?screenName=' + $scope.currentUser.screenName)
+            .then(function (result) {
+                if (result.data.length > 0) {
+                    $scope.currentUser = result.data[0];
+                    $scope.statusMessage = "";
+                }
+                else {
+                    $scope.statusMessage = "User not found";
+                    $scope.currentUser = null;
+                }
+            });
+	}
 }
 
